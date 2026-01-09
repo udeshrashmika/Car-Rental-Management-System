d99da641790616da50f6a3926e11038415bc2a17
Tharana10 <rehankahawita386@gmail.com>
2026-01-09 23:17:14 +0530

    updated RentalRepository

--- src/main/java/com/example/car_rental/repository/RentalRepository.java
+++ src/main/java/com/example/car_rental/repository/RentalRepository.java
@@ -1,10 +1,12 @@
 package com.example.car_rental.repository;
 
 import com.example.car_rental.model.Rental;
-import java.util.List;
-import com.example.car_rental.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Repository
 public interface RentalRepository extends JpaRepository<Rental, Long> {
@@ -12,4 +14,7 @@ public interface RentalRepository extends JpaRepository<Rental, Long> {
     List<Rental> findByStatus(String status);
 
     List<Rental> findByCustomerNameContainingIgnoreCase(String customerName);
+
+    @Query("SELECT SUM(r.totalCost) FROM Rental r")
+    BigDecimal getTotalRevenue();
 }
