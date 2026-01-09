0688469252f62c3e7e47f6516ce03dc60406b36d
Udesh <apurdesilva@students.nsbm.ac.lk>
2026-01-09 19:46:23 +0530

    Deleted RentalService

--- src/main/java/com/example/car_rental/controller/AdminController.java
+++ src/main/java/com/example/car_rental/controller/AdminController.java
@@ -4,7 +4,6 @@ import com.example.car_rental.model.Rental;
 import com.example.car_rental.model.Vehicle;
 import com.example.car_rental.repository.RentalRepository;
 import com.example.car_rental.repository.VehicleRepository;
-import com.example.car_rental.service.RentalService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
